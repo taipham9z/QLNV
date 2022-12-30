b05ba28a340d1b9502839a79d084e61d0b0e3bb0
taipham9z <taipham2104@gmail.com>
2022-12-30 14:58:30 +0700

    Test

--- src/Form/DuAnQL.java
+++ src/Form/DuAnQL.java
@@ -1,4 +1,19 @@
 package Form;
 
+import javax.swing.*;
+
 public class DuAnQL {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JComboBox comboBox1;
+    private JButton thêmButton;
+    private JButton sửaButton;
+    private JButton xoáButton;
+    private JButton resetButton;
+    private JButton đóngButton;
+    private JTable table1;
 }
